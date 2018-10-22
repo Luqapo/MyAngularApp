37f0a6b4ef5365a84aa468f9b669e90aa830d103
Lucjan Wiśnicz <luqapo@gmail.com>
2018-10-22 20:43:59 +0200

    filter

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent} from './custyomers-list.component/customers-list.component';
+import { FilterTextboxComponent } from './custyomers-list.component/filtered-textbox.component';
 
 
 @NgModule({
-  declarations: [ CustomersComponent, CustomersListComponent ],
+  declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
   imports: [ CommonModule ],
   exports: [ CustomersComponent ]
 })
